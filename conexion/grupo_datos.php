b32f0d9378926a5288dda643f6d716ee40689b57
Miguel Carmona <miguelcar18@gmail.com>
2014-08-22 02:02:22 -0430

    Edicion de opciones Guardado en localstorage
    Lo que se modifico fue mas que todo el archivo grupos_datos.php ubicado
    en la carpeta conexion

--- conexion/grupo_datos.php
+++ conexion/grupo_datos.php
@@ -6,23 +6,52 @@
     <?php
         include("funciones_mysql.php");
         conectar();
-        
+		
         foreach($_POST as $nombre_campo => $valor){ 
         $asignacion = "\$" . $nombre_campo . "='" . $valor . "';";
         eval($asignacion);
         if($valor != 'on' and $nombre_campo != 'hiddenid')
         {
             $verGrupoOpcion=str_replace('texto_','',$nombre_campo);
-            //$separar=explode('_',$verGrupoOpcion);
-            //$grupo=$separar[0];
-            //$opcion=$separar[1];
-            $stringingreso="insert into ctt_options_values "
-                . "(idOpcion, value, usuario_idusuario) "
-                . "values "
-                . "('".$verGrupoOpcion."',"
-                . "'".$valor."',"
-                . "'".$_POST['hiddenid']."')";
-        $sqlingreso=mysql_query($stringingreso) or die ("Error al ingresar los datos".mysql_error());
+			
+			$stringVerificar="select usuario_idusuario, idOpcion from ctt_options_values "
+			."where usuario_idusuario='".$_POST['hiddenid']."' and idOpcion='".$verGrupoOpcion."'";
+			$sqlVerificar=mysql_query($stringVerificar) or die ("Error en linea 19: ".mysql_error());
+			$cantidadVerificar=mysql_num_rows($sqlVerificar);
+			
+			if($cantidadVerificar==0 && $_POST['chck'.$verGrupoOpcion]=='on')
+			{
+				//$separar=explode('_',$verGrupoOpcion);
+				//$grupo=$separar[0];
+				//$opcion=$separar[1];
+				$stringingreso="insert into ctt_options_values "
+					. "(idOpcion, value, usuario_idusuario) "
+					. "values "
+					. "('".$verGrupoOpcion."',"
+					. "'".$valor."',"
+					. "'".$_POST['hiddenid']."')";
+				$sqlingreso=mysql_query($stringingreso) or die ("Error al ingresar los datos".mysql_error());
+				?>
+				<script>
+					var opcionesls = { '<?php echo $verGrupoOpcion?>' : '<?php echo $valor?>'};
+					localStorage.setItem('<?php echo $verGrupoOpcion?>', JSON.stringify(opcionesls));
+				</script>
+                <?php
+			}
+			else if ($cantidadVerificar > 0 && $_POST['chck'.$verGrupoOpcion]=='on')
+			{
+				$stringupdate="update ctt_options_values set "
+					. "value='".$valor."' "
+					. "where usuario_idusuario='".$_POST['hiddenid']."' and idOpcion='".$verGrupoOpcion."'";
+				$sqlupdate=mysql_query($stringupdate) or die ("Error al actualizar los datos: ".mysql_error());
+				?>
+				<script>
+					localStorage.removeItem('<?php echo $verGrupoOpcion?>');
+					var opcionesls = { '<?php echo $verGrupoOpcion?>' : '<?php echo $valor?>'};
+					localStorage.setItem('<?php echo $verGrupoOpcion?>', JSON.stringify(opcionesls));
+				</script>
+                <?php
+			}
         }
         
         }
