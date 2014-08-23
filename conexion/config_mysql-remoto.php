6f2ed65e6b8e9bf81e897916c9aa98186ec2a84a
Jonathan Araul <jonathan.araul@gmail.com>
2014-08-23 04:17:55 -0430

    Creacion de archivo de conexion remoto
    Miguel aunque no estoy usando este archivo en el proyecto no lo borres
    que es por si acaso borro el del servidor

---
+++ conexion/config_mysql-remoto.php
@@ -0,0 +1,10 @@
+<?php
+    //servidor
+    define ('HOST','localhost');
+    //usuario bd
+    define ('USER','hispanos_xcttsit');
+    //pass
+    define ('PASS','hispanos_xcttsite');
+    //base de datos
+    define ('DB','hispanos_xcttsite');
+?>
